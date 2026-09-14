7bc0b52c171d85f8f1f226b37f94628ca78583cd
Shivang <shivang.iitk@gmail.com>
2026-09-13 21:15:54 -0700

    style(auth): order coroutine imports

--- composeApp/src/desktopMain/kotlin/ai/rever/boss/components/auth/forms/DesktopAuthBrandSite.kt
+++ composeApp/src/desktopMain/kotlin/ai/rever/boss/components/auth/forms/DesktopAuthBrandSite.kt
@@ -25,8 +25,8 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.awaitCancellation
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.awt.Frame
